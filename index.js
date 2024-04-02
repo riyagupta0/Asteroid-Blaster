56adffacf20ce7d9a8d1c30848d3f01ad0ecd45c
riyagupta <guptariya9412@gmail.com>
2024-04-02 23:27:25 +0530

    adding canvas for the game

---
+++ index.js
@@ -0,0 +1,10 @@
+ const canvas = document.createElement("canvas");
+
+ document.querySelector(".myGame").appendChild(canvas);
+
+ canvas.width = innerWidth;
+ canvas.height = innerHeight;
+ const context = canvas.getContext("2d");
+
+
+
